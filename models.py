61b3ec6b3c6512e2045302a0b1dc0444948b2c0b
Sean <64774860+seany-web@users.noreply.github.com>
2020-08-01 17:01:26 +1200

    Initial commit

---
+++ models.py
@@ -0,0 +1,22 @@
+import datetime
+
+from peewee import *
+
+DATABASE = SqliteDatabase('learningjournal.db')
+
+class JournalEntry(Model):
+    title = CharField()
+    date_created = DateField()
+    time_spent = CharField()
+    content_learnt = TextField()
+    resources = TextField()
+
+    class Meta:
+        database = DATABASE
+        order_by = ('-date_created',)
+
+
+def initialise():
+    DATABASE.connect()
+    DATABASE.create_tables([JournalEntry], safe=True)
+    DATABASE.close()
